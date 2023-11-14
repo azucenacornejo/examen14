6e7d11e9150fd6d306f78b93ad510d0be7136ea7
Blanca <cornejoquispeazucena@gmail.com>
2023-11-14 18:31:34 -0500

    examen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import logo from './logo.svg';
 import './App.css';
-import componente from './componentes/componente';
+import C06componente from './component/C06componente';
+import C05contador from './component/C05contador';
+import C04doblecontador from './component/C04doblecontador';
+import C03variable from './component/C03variable';
+import C02operador from './component/C02operador';
+import C01matriz from './component/C01matriz';
 
-function App(){
+function App() {
   return (
     <div>
-      <componente/>
+    
+      <h1>HONORIO DELGADO ESPINOZA"</h1>
+      
+      <h2>Blanca Azucena Cornejo Quispe</h2>
+      
+      <h3>Turno: NOCHE VI SEMESTRE</h3>
+      
+      
+
+
     </div>
-
   );
-  }
+}
 
-  export default App;
+export default App;
