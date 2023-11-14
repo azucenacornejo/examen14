ab7fc7228936bd3147f6f16475b5ec8bb19a924d
blanca <cornejoquispeazucena@gmail.com>
2023-11-14 17:08:41 -0500

    examen actualizado

---
+++ src/componentes/Componente.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Componente = () => {
+  return (
+    <div>
+      <h1>Instituto Honorio Delgado Espinoza</h1>
+          <h2>Blanca Cornejo Quispe </h2>
+          <h3>Turno Noche</h3>
+    </div>
+  )
+}
+
+export default Componente
